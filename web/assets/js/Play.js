d844f33e910bed91a2c2306dc4ea4abf4c91e01c
Van Truong PHAN <svd.phan@gmail.com>
2019-05-13 16:40:56 +0200

    spread operation

--- web/assets/js/Play.js
+++ web/assets/js/Play.js
@@ -27,3 +27,20 @@ const aGreatObject = new AnotherGreatClass(42, 'adventure');
 console.log(
   aGreatObject.returnGreatThings()
 );
+
+
+
+// spread operator
+
+let printThreeThings = function (thing1, thing2, thing3) {
+  console.log(thing1, thing2, thing3)
+}
+
+let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
+printThreeThings(...yummyThings);
+
+let greatThings = ['swimming', 'sunsets', ...yummyThings];
+let anotherGreatThings = [...greatThings];
+anotherGreatThings.push('summer');
+console.log(greatThings);
+console.log(anotherGreatThings);
